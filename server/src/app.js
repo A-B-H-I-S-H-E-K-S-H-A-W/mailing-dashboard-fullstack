c63250c6241488998a66b46a302eeacf2df03cbe
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-08-20 19:04:34 +0530

    Added origin

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,11 @@ import { emailRouter } from "./routes/email.routes.js";
 
 export const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://mailing-dashboard.vercel.app/",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/v1/auth", authRouter);
